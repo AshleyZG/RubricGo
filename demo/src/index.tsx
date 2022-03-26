0e8ba394b6821c150adb54e38cdd9153fb183d2d
AshleyZG <gsarazh@gmail.com>
2022-03-26 19:43:54 -0400

    routes in sidebar

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -6,33 +6,57 @@ import reportWebVitals from './reportWebVitals';
 import { ClusterApp } from './ClusterApp';
 import { CheckApp } from './CheckApp';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-// import { Page1, Page2, Page3 } from './Test';
-function ContentRouter(){
+
+function ContentRoutes(){
   return (
-    <Router>
     <Routes>
-      <Route path="" element={<App/>}></Route>
-      <Route path="cluster" element={<ClusterApp/>}></Route>
-      <Route path="check" element={<CheckApp/>}></Route>
-      <Route path="/debug" element={<div />}></Route>
-      {/* <Route path='p1' element={<Page1/>}></Route>
-      <Route path='p2' element={<Page2/>}></Route>
-      <Route path='p3' element={<Page3/>}></Route> */}
-    </Routes>
-  </Router>
+    <Route path="" element={<App/>}></Route>
+    <Route path="/cluster" element={<ClusterApp/>}></Route>
+    <Route path="/check" element={<div />}></Route>
+  </Routes>
+
+)
+}
+
+interface Item{
+  name: string;
+  route: string;
+  key?: number;
+}
+const sidebarItems: Item[] = [
+  {
+    name: "Cluster Submissions",
+    route: "/cluster"
+  },
+  {
+    name: "Check and Regrade",
+    route: "/check"
+  },
+];
+
+function SidebarItem(props: Item){
+  return (
+    <Link to={props.route} key={props.key}>
+      <p>{props.name}</p>
+    </Link>
   )
 }
 
 function Sidebar(){
-  return (<div>
+  return (<div id="sidebar">
     <h1>Sidebar</h1>
+    {sidebarItems.map((value: Item, index: number) => {
+      return <SidebarItem  name={value.name} route={value.route} key={index}/>;
+    })}
   </div>)
 }
 
 function Layout(){
-  return (<div>
-    <Sidebar/>
-    <ContentRouter/>
+  return (<div id="layout">
+    <Router>
+      <Sidebar/>
+      <ContentRoutes/>
+    </Router>
   </div>)
 }
 
