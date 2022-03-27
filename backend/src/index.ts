72430777d914e8c0d90db130278092a973191ba7
AshleyZG <gsarazh@gmail.com>
2022-03-27 14:13:56 -0400

    send cluster results to frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
-
-   
 import * as path from 'path';
 import express, { request, response } from 'express';
 import * as fs from 'fs';
 import cors from 'cors';
 import { spawnSync } from 'child_process';
+import { parse } from 'csv-parse';
+
 
 const allowedOrigins = ['http://localhost:3000'];
 
@@ -20,6 +20,36 @@ const PORT = 5000;
 
 app.get('/debug', (request, response) => {
     response.send('hello')
+})
+
+app.get('/clusterResult', (request, response) => {
+    const resultsPath = path.resolve(__dirname, '../../data/cluster.csv');
+    console.log(resultsPath);
+    const headers = ["id", 'text', 'agg_bert_row'];
+    const fileContent = fs.readFileSync(resultsPath );
+    var content;
+    parse(fileContent, {
+        delimiter: ',',
+        columns: headers,
+      }, (error, result) => {
+        if (error) {
+          console.error(error);
+        }
+    
+        // console.log("Result", result);
+        // console.log(result[0] as AnswerItem]);
+        result.shift();
+        result.forEach((value: any) => {
+            value.id = parseInt(value.id);
+            value.agg_bert_row = parseInt(value.agg_bert_row);
+        })
+        content = result;
+        response.send({clusteredAnswers: content})
+        // console.log(content);
+    });
+    // console.log(content);
+
+
 })
 
 app.listen(PORT, () => {
