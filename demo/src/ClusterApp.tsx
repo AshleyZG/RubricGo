41f80f1c948e520354414115ab0d4c01765694bf
AshleyZG <gsarazh@gmail.com>
2022-03-26 21:09:37 -0400

    cluster app modules

--- demo/src/ClusterApp.tsx
+++ demo/src/ClusterApp.tsx
@@ -9,16 +9,30 @@ class ClusterApp extends React.Component {
 
     render(): React.ReactNode {
         return <div className='view'>
+            <div className='title'>
                 <h1>{"Step 1:  Design Rubric Based on Clustering"}</h1>
+            </div>
+            <div className='content'>
                 <div id="cluster">
                     Todo: display clustering results
+                    <div id="range-slider">
+                        Todo: add a range slider here
+                    </div>
+                    <div id="cluster-viz">
+                        Todo: add cluster visualization
+                    </div>
                 </div>
-                <div id="rubric-design">
-                    Todo: rubric design
+                <div id="cluster-analysis">
+                    <div id="rubric-design">
+                        Todo: rubric design
+                    </div>
+                    <div id="overview">
+                        Todo: overview
+                    </div>
+
                 </div>
-                <div id="overview">
-                    Todo: overview
-                </div>
+
+            </div>
         </div>
     }
 }
